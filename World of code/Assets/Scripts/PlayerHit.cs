d635fadeae9683b0b28e05d7214190fa3231264c
frederikja163 <fjaplv64@gmail.com>
2018-08-28 22:06:34 +0200

    t

---
+++ World of code/Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+    public Animator animator;
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+		animator.SetBool("Hitting", Input.GetMouseButton(0));
+	}
+}
